22decf59309d5a4569228c9a3d3b994125dd9454
Andrey <kalashnikov3434@gmail.com>
2024-05-22 12:07:16 +0300

    no message

--- app/src/main/java/com/kalashnikov/tt_fooddelivery_1/data/AppData.kt
+++ app/src/main/java/com/kalashnikov/tt_fooddelivery_1/data/AppData.kt
@@ -39,7 +39,7 @@ object AppData {
         listOf("Все меню")
     )
 
-    // Данные для категории и диалога
+    // Данные для категории и диалога dfgfhfgh
     val id = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8)
 
     val image_menu = listOf(
